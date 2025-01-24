319f3e5b169286e59b128a0933e66d7b610c5c28
Lee Seonghun <lsh0641@gmail.com>
2025-01-24 17:54:47 +0900

    반응형 레이아웃 웹 완성
    다음에 font 전부 다 rem으로 바꾸고 min-width 값 설정해주기

--- js/style.js
+++ js/style.js
@@ -1,11 +1,15 @@
-window.onload = function(){
-    const visualSlide = new Swiper(".daegun_banner", {
-        slidesPerView : 1,
-        slidesPerGroup : 1,
-        loop : true,
-        //autoplay: {
-           // delay: 2500,
-          //  disableOnInteraction: false,
-        //},
-    })
-}
+window.onload = function () {
+  const visualSlide = new Swiper(".daegun_banner", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    //autoplay: {
+    // delay: 2500,
+    //  disableOnInteraction: false,
+    //},
+  });
+  const newsSlide = new Swiper(".school_news", {
+    slidesPerView: 3,
+    spaceBetween : 100,
+  });
+};
