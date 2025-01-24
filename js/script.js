bb619724bf7e2217b0ed9b875e3a2f98f8ee23b6
Lee Seonghun <lsh0641@gmail.com>
2025-01-24 16:04:01 +0900

    중간저장

--- js/script.js
+++ js/script.js
@@ -9,4 +9,8 @@ $(document).ready(function () {
       $(this).find(".depth2").stop().slideUp();
     }
   );
+  $(".c1 h4").click(function(){
+    $(".c1 h4, .c1 ul").removeClass("on")
+    $(this).addClass("on").next("ul").addClass("on")
+  })
 });
--- js/style.js
+++ js/style.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     const visualSlide = new Swiper(".daegun_banner", {
         slidesPerView : 1,
+        slidesPerGroup : 1,
         loop : true,
         //autoplay: {
            // delay: 2500,
